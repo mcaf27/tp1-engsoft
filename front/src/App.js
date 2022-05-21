23cf0144263b20469f9ee1ecad3d68079f5b8927
mcaf27 <claraferr27@gmail.com>
2022-05-21 15:01:59 -0300

    rotas corrigidas

--- front/src/App.js
+++ front/src/App.js
@@ -1,22 +1,59 @@
+import { useState } from 'react';
+
 import './global.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import PublicRoute from './components/routes/PublicRoute';
+import PrivateRoute from './components/routes/PrivateRoute';
+
+import Login from './pages/login';
+import Books from './pages/books';
+import Wishlist from './pages/wishlist';
+
+import Navbar from './components/navbar';
 
 function App() {
+  const [isLogged, setIsLogged] = useState(true);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route
+            path="/login"
+            element={
+              <PublicRoute isLogged={isLogged}>
+                <Login />
+              </PublicRoute>
+            }
+          />
+          <Route
+            path="/"
+            element={
+              <PrivateRoute isLogged={isLogged}>
+                <Books />
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path="/meus-livros"
+            element={
+              <PrivateRoute isLogged={isLogged}>
+                <Books />
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path="/lista-de-desejos"
+            element={
+              <PrivateRoute isLogged={isLogged}>
+                <Wishlist />
+              </PrivateRoute>
+            }
+          />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
