4dd11485ced60ff9b1e886dcd722d3fe5039a363
Kaio <desakaio4@gmail.com>
2022-11-06 16:43:26 -0300

    refatoração doo método Navbar@15-71@./frontend\src\components\navbar\index.js

--- frontend/src/components/navbar/index.js
+++ frontend/src/components/navbar/index.js
@@ -12,6 +12,41 @@ const Dot = styled('span')(({ theme }) => ({
   bottom: '0.5rem',
 }));
 
+
+function LoggedMenuItems() {
+  const { pathname } = useLocation();
+  const theme = useTheme();
+
+  return (
+    <>
+      <MenuItem
+        sx={{
+          borderBottom:
+            pathname === '/livros' || pathname === '/'
+              ? `2px solid ${theme.palette.text.primary}`
+              : '0',
+          mr: 2,
+        }}
+      >
+        <Link style={{ padding: `${theme.spacing(3)} 0` }} to="/livros">
+          livros
+        </Link>
+      </MenuItem>
+
+      <MenuItem
+        sx={{
+          borderBottom:
+            pathname === '/minha-biblioteca' ? `2px solid ${theme.palette.text.primary}` : '0',
+        }}
+      >
+        <Link to="/minha-biblioteca" style={{ padding: `${theme.spacing(3)} 0` }}>
+          Biblioteca de {localStorage.getItem('Email')}
+        </Link>
+      </MenuItem>
+    </>
+  );
+}
+
 export default function Navbar() {
   const { pathname } = useLocation();
   const theme = useTheme();
@@ -23,37 +58,10 @@ export default function Navbar() {
       <Container maxWidth="xl">
         <Toolbar>
           {isLogged && (
-            <>
-              <MenuItem
-                sx={{
-                  borderBottom:
-                    pathname === '/livros' || pathname === '/'
-                      ? `2px solid ${theme.palette.text.primary}`
-                      : '0',
-                  mr: 2,
-                }}
-              >
-                <Link style={{ padding: `${theme.spacing(3)} 0` }} to="/livros">
-                  livros
-                </Link>
-              </MenuItem>
-
-              <MenuItem
-                sx={{
-                  borderBottom:
-                    pathname === '/minha-biblioteca'
-                      ? `2px solid ${theme.palette.text.primary}`
-                      : '0',
-                }}
-              >
-                <Link to="/minha-biblioteca" style={{ padding: `${theme.spacing(3)} 0` }}>
-                  Biblioteca de  { localStorage.getItem("Email")}
-                </Link>
-              </MenuItem>
-            </>
+            <LoggedMenuItems/>
           )}
 
-          <MenuItem component={Link} to="/" sx={{ ml: 'auto', my: isLogged ? 1 : 3 }}>
+          <MenuItem component={Link} to="/" sx={{ ml: 'auto', my: isLogged ? 1 : 3 }}> 
             <Typography
               variant="h1"
               sx={{
@@ -71,3 +79,4 @@ export default function Navbar() {
     </AppBar>
   );
 }
+
